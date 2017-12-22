576e47f2035d95f9cc0d8375352eea7ac7d6f76c
Angel Rivera <arivera@microshaare.io>
2017-12-22 16:21:22 -0500

    updated the version number

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=hab_flask
 pkg_origin=ariv3ra
-pkg_version="0.1.0"
+pkg_version="0.1.2"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 # pkg_upstream_url="https://github.com/ariv3ra/gustos"
